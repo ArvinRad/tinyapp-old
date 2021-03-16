dc77e27211bc8ee8a476b13c248879aaa0f921d0
ArvinRad <radanarvin@gmail.com>
2021-03-16 07:47:08 +0000

    Added Random String Generator and body parser

--- express_server.js
+++ express_server.js
@@ -10,6 +10,9 @@ const urlDatabase = {
   "S152tx": "https://www.tsn.ca"
 }
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 })
@@ -19,6 +22,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+function generateRandomString() {
+  let alphaNumericStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz"; 
+  let randomStr = [];
+  for (let i =0; i < 6; i++) {
+    randomStr.push(alphaNumericStr.charAt(Math.trunc(alphaNumericStr.length * Math.random())));
+    return randomStr.joint('');
+  }
+
+}
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -27,6 +49,11 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
